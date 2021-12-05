fcdf1ac397dfe8e32da86cc941a17ba1074e22b2
Jesse Zheng <zheng.1708@osu.edu>
2021-12-05 13:12:26 -0500

    trying to recreate grade model

--- app/models/grade.rb
+++
@@ -1,6 +0,0 @@
-class Grade < ApplicationRecord
-  validates :score, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  belongs_to :student, class_name: 'user'
-  has_one :project
-end
