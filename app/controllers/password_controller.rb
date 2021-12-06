bef9005d9faea6480a7f57ec27cf5e3b7bbaa731
jeonso0907 <jeonso0907@gmail.com>
2021-12-05 20:37:21 -0500

    all reset procedures are done, except updating the password and handling error inputs

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -3,27 +3,27 @@ class PasswordController < ApplicationController
   end
 
   def reset
-    @user = User.find_by_email("jeonso3412@gmail.com")
+    @user = User.find_by_email(request.params[:email])
   end
 
   def mail_reset
-    email_to_reset = [:email]
+    email_to_reset = params[:email]
     url = reset_password_url
     PasswordMailer.reset_password(email_to_reset, url).deliver_now
+    redirect_to login_url
   end
 
   def reset_password
-    @user = User.find_by_email(params[:email])
     current_password = params[:user][:current_password]
 
     if @user
       new_password = params[:new_password]
       confirm_password = params[:confirm_password]
-      if @user.update(password: new_password, confirm_password: confirm_password)
+      if @user.update_attribute(password: new_password)
         flash[:success] = "The password has been updated"
-        # redirect_to login_url
-      # else
-      #   render 'reset'
+        redirect_to login_url
+      else
+        render 'reset'
       end
     end
   end
--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,9 +2,9 @@ class PasswordMailer < ApplicationMailer
     default from: 'bitplease-rating@osu.edu'
 
     def reset_password(email_to_reset, url)
-        email = "jeonso3412@gmail.com"
-        @url = url
-        mail(to: email, subject: 'Reset Password')
+        @email = email_to_reset
+        @url = url + "?email=" + @email
+        mail(to: @email, subject: 'Reset Password')
     end
 
     def reset
