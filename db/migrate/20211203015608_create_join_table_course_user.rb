63283d549e8745120d2f18fd63adff72862604bd
Sooyoung <jeonso3412@gmail.com>
2021-12-06 09:19:08 -0500

    Deleted db/migrate to get merged with main

--- db/migrate/20211203015608_create_join_table_course_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCourseUser < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :courses, :users do |t|
-      # t.index [:course_id, :user_id]
-      # t.index [:user_id, :course_id]
-    end
-  end
-end
