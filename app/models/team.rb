5cd73383d1fa6056db1cd997567156492f541c03
Jesse Zheng <zheng.1708@osu.edu>
2021-12-06 15:36:10 -0500

    instructors and TAs can only grade their students

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,11 @@ class Team < ApplicationRecord
         @p=Project.find_by(id: project_id)
         @p.project_name unless @p.nil?
     end
+    def course_no
+        @p=Project.find_by(id: project_id)
+        @c=Course.find_by(id: @p.course_id)
+        @c.course_no unless @c.nil?
+    end
 
     def course_users
         #User.where(user_role: "Student") unless project_id.nil?
@@ -16,5 +21,14 @@ class Team < ApplicationRecord
         @users=@course.users.where(user_role: "Student");
 
     end
+
+    def course_admins
+        #User.where(user_role: "Student") unless project_id.nil?
+        #byebug
+        @project=Project.find_by(id: project_id)
+        @course=Course.find_by(id: @project.course_id)
+        @admins=@course.users.where(user_role: ["Instructor","Teaching Assitant"]);
+    end
+
 end
 
